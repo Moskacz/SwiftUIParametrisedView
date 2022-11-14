6530fdbcc4b32218951fe2878beaf1604559bc47
Michał Moskała <mokm@viessmann.com>
2022-11-14 20:18:38 +0100

    Initial commit

---
+++ SwiftUIParametrisedView/SwiftUIParametrisedViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIParametrisedViewApp.swift
+//  SwiftUIParametrisedView
+//
+//  Created by Michal Moskala on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIParametrisedViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
